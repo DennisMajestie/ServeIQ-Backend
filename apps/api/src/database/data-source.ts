6449ac8e20fe04b94a27ed22942cf73b396e4ba8
DennisMaj <charlesgift231@gmail.com>
2026-06-29 18:20:35 +0100

    Fix paths for production migrations

--- apps/api/src/database/data-source.ts
+++ apps/api/src/database/data-source.ts
@@ -4,10 +4,10 @@ import { config } from 'dotenv';
 config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  synchronize: false,
-  logging: true,
-  entities: [process.env.NODE_ENV === 'production' ? 'dist/**/*.entity.js' : 'src/**/*.entity.ts'],
-  migrations: [process.env.NODE_ENV === 'production' ? 'dist/apps/api/src/database/migrations/*.js' : 'src/database/migrations/*.ts'],
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: true,
+    entities: [process.env.NODE_ENV === 'production' ? 'dist/modules/**/*.entity.js' : 'src/modules/**/*.entity.ts'],
+    migrations: [process.env.NODE_ENV === 'production' ? 'dist/database/migrations/*.js' : 'src/database/migrations/*.ts'],
 });
